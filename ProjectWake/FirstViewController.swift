f463a22a3199b32248138b687c56b5daeada117b
Nicholas Sabelli <nicksabelli1694@gmail.com>
2015-02-21 12:00:58 -0500

    Initial Commit

---
+++ ProjectWake/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ProjectWake
+//
+//  Created by Nicholas Sabelli on 2015-02-21.
+//  Copyright (c) 2015 Nicholas Sabelli. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
